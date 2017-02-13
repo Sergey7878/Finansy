ff704b4a8f729f32f0531e4a960e6a0fede3d527
Sergey <yudin.ser.vl@gmail.com>
2017-02-13 22:13:06 +0300

    Закончил в 2.05 метод OnDistroy

---
+++ app/src/main/java/com/example/sergey/finansy/MainActivity2.java
@@ -0,0 +1,17 @@
+package com.example.sergey.finansy;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Sergey on 13.02.2017.
+ */
+
+public class MainActivity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_2);
+    }
+}
